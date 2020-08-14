c6aaf4132741f1f21ca0b5d872f8ab1b6a56c580
dilini1995s <dilinitharanga721@gmail.com>
2020-08-14 20:53:41 +0530

    second commit

--- src/app/pages/aaibtab1/aaibtab1.page.ts
+++ src/app/pages/aaibtab1/aaibtab1.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router,ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-aaibtab1',
   templateUrl: './aaibtab1.page.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Aaibtab1Page implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+  back(){
+    this.router.navigate(['/login']);
+  }
 }
